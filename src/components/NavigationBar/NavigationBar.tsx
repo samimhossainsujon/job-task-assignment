0d5a77fcd44a6ecbeb93ca5e5a032226ee17e8f5
MD SAMIM HOSSAIN SUJON <samimhossainsanto@gmail.com>
2024-04-02 22:56:31 +0600

    navigation bar add

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,8 +2,31 @@ import React from "react";
 
 const NavigationBar = () => {
   return (
-    <div>
-      <h1>NavigationBar</h1>
+    <div className="bg-[#222052] text-white">
+      <div className="flex justify-between p-5">
+       
+        <div className="bg-white text-[#222052] rounded-3xl p-5 ">
+          <h1 className="font-bold underline decoration-4">ASSIGNMENT</h1>
+          <h1 className="text-justify">learn it, do it</h1>
+        </div>
+
+        <div className="flex gap-10">
+          <h1>HOME</h1>
+          <h1>ABOUT</h1>
+          <h1>COURSE</h1>
+          <h1>EVENTS</h1>
+          <h1>CONTRACT</h1>
+        </div>
+
+        <div className="flex gap-5">
+          <h1>Secarch</h1>
+          <h1>profile</h1>
+        </div>
+      </div>
+
+      <div className="text-center">
+        <h1>TETHERS PROFILE</h1>
+      </div>
     </div>
   );
 };
